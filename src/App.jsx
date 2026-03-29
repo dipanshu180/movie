703751e452faebcddd5f8fea3950832b434d0bb9
dipanshu180 <dipanshu180503@gmail.com>
2026-03-29 19:25:11 +0530

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,24 @@ import Home from './pages/Home';
 import Fav from './pages/Fav';
 import MovieCard from './Component/MovieCard' 
 import { Routes , Route } from 'react-router-dom';
-function App() {
+import Navbar from './Component/Navbar';
+function App() { 
   
-
-  const movieNumber = 1;
   return (
     <>
 
       {/* {movieNumber===1 && <MovieCard movie={{title:"Dhurander" ,release_date:2026}}/>} */}
-      <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/fav' element={<Fav/>}/>
-      </Routes>
+      <div>
+        <Navbar/>
+        
+        <main>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/fav' element={<Fav/>}/>
+
+        </Routes>
+        </main>
+      </div>
         
     </>
   )
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+function Navbar(){
+    return(
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <Link to="/">Movie App</Link>
+            </div>
+            <div className="navbar-nav">
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/fav" className="nav-link">Favorites</Link>
+            </div>
+        </nav>
+    )
+
+}
+
+export default Navbar;
