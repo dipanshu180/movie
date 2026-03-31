b3ff7b08769c1335b469f5ec5ecede6bd80b13dc
dipanshu180 <dipanshu180503@gmail.com>
2026-04-01 01:55:28 +0530

    Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,76 @@
-import MovieCard from "../Component/MovieCard"
-import { useState , useEffect, use} from "react";
+import MovieCard from "../component/MovieCard";
+import { useState, useEffect } from "react";
 import { searchMovies , getPopularMovies } from "../service/api";
-import '../css/Home.css'
+import "../css/Home.css";
 
-function Home(){
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const  [movies, setMovies] = useState([]);
-    const [searchQuery , setSearchQuery] = useState("");
-    const [loading , setLoading] = useState(false);
-    const [error , setError] = useState(null);
-
-    useEffect(()=> {
-        const loadMovies = async () =>{
-            try{
-                const popularMovies = await getPopularMovies();
-                setMovies(popularMovies);
-            }
-            catch(error){
-                console.error("Error fetching movies:", error);
-                setError(error);
-            }
-            finally{
-                setLoading(false);
-        }
-    }
-    loadMovies();
-    } , [])
-
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-        alert(searchQuery)
-        setSearchQuery("")
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.log(err);
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
+      }
     };
-    return(
-        <div className="home">
 
-            <form onSubmit={handleSubmit} className="search-form">
-                <input type="text" placeholder="Search your movie..."  value={searchQuery} onChange={e => setSearchQuery(e.target.value)}/>
-                <button type="submit" className="search-button">Search</button>
-            </form>
+    loadPopularMovies();
+  }, []);
 
-            {error && <div className="error">Error: {error.message}</div>}
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (!searchQuery.trim()) return
+    if (loading) return
 
-            {loading ? (<div className="loading">Loading...</div> ):( 
-            <div className="movie-grid">{movies.map(movie => <MovieCard movie ={movie} key = {movie.id}/>)}</div>)}
+    setLoading(true)
+    try {
+        const searchResults = await searchMovies(searchQuery)
+        setMovies(searchResults)
+        setError(null)
+    } catch (err) {
+        console.log(err)
+        setError("Failed to search movies...")
+    } finally {
+        setLoading(false)
+    }
+  };
 
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
+        {error && <div className="error-message">{error}</div>}
+
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- src/service/api.js
+++ src/service/api.js
@@ -1,4 +1,4 @@
-const API_KEY=""
+const API_KEY="eaddb37705283954a8759274b05885e9"
 const BASE_URL="https://api.themoviedb.org/3"
 
 export const getPopularMovies = async () =>{
